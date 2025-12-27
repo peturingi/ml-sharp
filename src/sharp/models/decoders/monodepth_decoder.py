2aab8c4668483ffaf0cc50db2dd8d75f9dedc4cd
Pétur Ingi Egilsson <petur@ingi.dk>
2025-12-27 19:44:14 +0100

    Revert "Added typehints."
    This reverts commit bd08f52e12ac069903670d06f51bd9e8085b3eae.

--- src/sharp/models/decoders/monodepth_decoder.py
+++ src/sharp/models/decoders/monodepth_decoder.py
@@ -7,8 +7,6 @@ Copyright (C) 2025 Apple Inc. All Rights Reserved.
 
 from __future__ import annotations
 
-from typing import Final
-
 from sharp.models.presets import (
     MONODEPTH_ENCODER_DIMS_MAP,
     ViTPreset,
@@ -27,10 +25,13 @@ def create_monodepth_decoder(
         patch_encoder_preset: The preset patch encoder architecture in SPN.
         dims_decoder: The decoder architecture.
     """
-    dims_encoder: Final[list[int] | int] = MONODEPTH_ENCODER_DIMS_MAP[patch_encoder_preset]
+    dims_encoder = MONODEPTH_ENCODER_DIMS_MAP[patch_encoder_preset]
+    if dims_decoder is None:
+        dims_decoder = dims_encoder[0]
     if isinstance(dims_decoder, int):
         dims_decoder = [dims_decoder]
-
-    return MultiresConvDecoder(
+    decoder = MultiresConvDecoder(
         dims_encoder=[dims_decoder[0]] + list(dims_encoder), dims_decoder=dims_decoder
     )
+
+    return decoder
