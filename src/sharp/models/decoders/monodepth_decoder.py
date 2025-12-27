fbeff3f079d9361caa002221e4856cc591052088
Pétur Ingi Egilsson <petur@ingi.dk>
2025-12-27 19:44:14 +0100

    Revert "Added typehints."
    This reverts commit 28195d62eccb3bb52c85fd1c7a4eaf06c63afdcf.

--- src/sharp/models/decoders/monodepth_decoder.py
+++ src/sharp/models/decoders/monodepth_decoder.py
@@ -7,7 +7,6 @@ Copyright (C) 2025 Apple Inc. All Rights Reserved.
 
 from __future__ import annotations
 
-from collections.abc import Sequence
 from typing import Final
 
 from sharp.models.presets import (
@@ -20,7 +19,7 @@ from .multires_conv_decoder import MultiresConvDecoder
 
 def create_monodepth_decoder(
     patch_encoder_preset: ViTPreset,
-    dims_decoder: int | Sequence[int] | None=None,
+    dims_decoder=None,
 ) -> MultiresConvDecoder:
     """Create DepthDensePredictionTransformer model.
 
