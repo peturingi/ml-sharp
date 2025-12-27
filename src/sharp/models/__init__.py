ead6135689e3c7970982d15d7909f85084f51f5a
Pétur Ingi Egilsson <petur@ingi.dk>
2025-12-27 18:14:21 +0100

    Fixed a linting error due to 'params' being shadows.

--- src/sharp/models/__init__.py
+++ src/sharp/models/__init__.py
@@ -20,44 +20,44 @@ from .params import PredictorParams
 from .predictor import RGBGaussianPredictor
 
 
-def create_predictor(params: PredictorParams) -> RGBGaussianPredictor:
+def create_predictor(predictor_params: PredictorParams) -> RGBGaussianPredictor:
     """Create gaussian predictor model specified by name."""
-    if params.gaussian_decoder.stride < params.initializer.stride:
+    if predictor_params.gaussian_decoder.stride < predictor_params.initializer.stride:
         raise ValueError(
             "We do not expected gaussian_decoder has higher resolution than initializer."
         )
 
-    scale_factor = params.gaussian_decoder.stride // params.initializer.stride
+    scale_factor = predictor_params.gaussian_decoder.stride // predictor_params.initializer.stride
     gaussian_composer = GaussianComposer(
-        delta_factor=params.delta_factor,
-        min_scale=params.min_scale,
-        max_scale=params.max_scale,
-        color_activation_type=params.color_activation_type,
-        opacity_activation_type=params.opacity_activation_type,
-        color_space=params.color_space,
+        delta_factor=predictor_params.delta_factor,
+        min_scale=predictor_params.min_scale,
+        max_scale=predictor_params.max_scale,
+        color_activation_type=predictor_params.color_activation_type,
+        opacity_activation_type=predictor_params.opacity_activation_type,
+        color_space=predictor_params.color_space,
         scale_factor=scale_factor,
-        base_scale_on_predicted_mean=params.base_scale_on_predicted_mean,
+        base_scale_on_predicted_mean=predictor_params.base_scale_on_predicted_mean,
     )
-    if params.num_monodepth_layers > 1 and params.initializer.num_layers != 2:
+    if predictor_params.num_monodepth_layers > 1 and predictor_params.initializer.num_layers != 2:
         raise KeyError("We only support num_layers = 2 when num_monodepth_layers > 1.")
 
-    monodepth_model = create_monodepth_dpt(params.monodepth)
+    monodepth_model = create_monodepth_dpt(predictor_params.monodepth)
     monodepth_adaptor = create_monodepth_adaptor(
         monodepth_model,
-        params.monodepth_adaptor,
-        params.num_monodepth_layers,
-        params.sorting_monodepth,
+        predictor_params.monodepth_adaptor,
+        predictor_params.num_monodepth_layers,
+        predictor_params.sorting_monodepth,
     )
 
-    if params.num_monodepth_layers == 2:
-        monodepth_adaptor.replicate_head(params.num_monodepth_layers)
+    if predictor_params.num_monodepth_layers == 2:
+        monodepth_adaptor.replicate_head(predictor_params.num_monodepth_layers)
 
     gaussian_decoder = create_gaussian_decoder(
-        params.gaussian_decoder,
+        predictor_params.gaussian_decoder,
         dims_depth_features=monodepth_adaptor.get_feature_dims(),
     )
     initializer = create_initializer(
-        params.initializer,
+        predictor_params.initializer,
     )
     prediction_head = DirectPredictionHead(
         feature_dim=gaussian_decoder.dim_out, num_layers=initializer.num_layers
@@ -69,7 +69,7 @@ def create_predictor(params: PredictorParams) -> RGBGaussianPredictor:
         prediction_head=prediction_head,
         monodepth_model=monodepth_adaptor,
         gaussian_composer=gaussian_composer,
-        scale_map_estimator=create_alignment(params.depth_alignment, depth_decoder_dim=decoder_dim),
+        scale_map_estimator=create_alignment(predictor_params.depth_alignment, depth_decoder_dim=decoder_dim),
     )
 
 
