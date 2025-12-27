20252635c287dc250b5cd40594e6afe1b00d2739
Pétur Ingi Egilsson <petur@ingi.dk>
2025-12-27 18:19:25 +0100

    Added typehints.

--- src/sharp/models/__init__.py
+++ src/sharp/models/__init__.py
@@ -6,16 +6,18 @@ Copyright (C) 2025 Apple Inc. All Rights Reserved.
 
 from __future__ import annotations
 
+from typing import Final
+
 from sharp.models.monodepth import (
     create_monodepth_adaptor,
-    create_monodepth_dpt,
+    create_monodepth_dpt, MonodepthDensePredictionTransformer, MonodepthWithEncodingAdaptor,
 )
 
 from .alignment import create_alignment
 from .composer import GaussianComposer
-from .gaussian_decoder import create_gaussian_decoder
+from .gaussian_decoder import create_gaussian_decoder, GaussianDensePredictionTransformer
 from .heads import DirectPredictionHead
-from .initializer import create_initializer
+from .initializer import create_initializer, MultiLayerInitializer
 from .params import PredictorParams
 from .predictor import RGBGaussianPredictor
 
@@ -27,8 +29,8 @@ def create_predictor(predictor_params: PredictorParams) -> RGBGaussianPredictor:
             "We do not expected gaussian_decoder has higher resolution than initializer."
         )
 
-    scale_factor = predictor_params.gaussian_decoder.stride // predictor_params.initializer.stride
-    gaussian_composer = GaussianComposer(
+    scale_factor: Final[int] = predictor_params.gaussian_decoder.stride // predictor_params.initializer.stride
+    gaussian_composer: Final[GaussianComposer] = GaussianComposer(
         delta_factor=predictor_params.delta_factor,
         min_scale=predictor_params.min_scale,
         max_scale=predictor_params.max_scale,
@@ -41,8 +43,8 @@ def create_predictor(predictor_params: PredictorParams) -> RGBGaussianPredictor:
     if predictor_params.num_monodepth_layers > 1 and predictor_params.initializer.num_layers != 2:
         raise KeyError("We only support num_layers = 2 when num_monodepth_layers > 1.")
 
-    monodepth_model = create_monodepth_dpt(predictor_params.monodepth)
-    monodepth_adaptor = create_monodepth_adaptor(
+    monodepth_model: Final[MonodepthDensePredictionTransformer] = create_monodepth_dpt(predictor_params.monodepth)
+    monodepth_adaptor: Final[MonodepthWithEncodingAdaptor] = create_monodepth_adaptor(
         monodepth_model,
         predictor_params.monodepth_adaptor,
         predictor_params.num_monodepth_layers,
@@ -52,14 +54,14 @@ def create_predictor(predictor_params: PredictorParams) -> RGBGaussianPredictor:
     if predictor_params.num_monodepth_layers == 2:
         monodepth_adaptor.replicate_head(predictor_params.num_monodepth_layers)
 
-    gaussian_decoder = create_gaussian_decoder(
+    gaussian_decoder: Final[GaussianDensePredictionTransformer] = create_gaussian_decoder(
         predictor_params.gaussian_decoder,
         dims_depth_features=monodepth_adaptor.get_feature_dims(),
     )
-    initializer = create_initializer(
+    initializer: Final[MultiLayerInitializer] = create_initializer(
         predictor_params.initializer,
     )
-    prediction_head = DirectPredictionHead(
+    prediction_head: Final[DirectPredictionHead] = DirectPredictionHead(
         feature_dim=gaussian_decoder.dim_out, num_layers=initializer.num_layers
     )
     decoder_dim = monodepth_model.decoder.dims_decoder[-1]
